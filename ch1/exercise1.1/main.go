682f065a470781c51a9b2722021f324d21b43d22
mbraeutig <m.braeutigam@posteo.de>
2018-01-02 20:33:10 +0100

    ...

--- ch1/exercise1.1/main.go
+++ ch1/exercise1.1/main.go
@@ -7,16 +7,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Printf("Command:%s\n", os.Args[0])
-
-	var s, sep string
-
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	}
-	fmt.Println(s)
+	fmt.Println(strings.Join(os.Args[0:], " "))
 }
